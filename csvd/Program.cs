5573e1fdbd67c728a348dd9515326e8b9c07cce8
LC9er <liquidcloud9@gmail.com>
2022-06-22 20:51:33 -0400

    read args

--- csvd/Program.cs
+++ csvd/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using CsvHelper;
+
+namespace csvd 
+{
+    public class csvd
+    {
+        static void Main(string[] args)
+        {
+            /// Parse args - file1 file2 key
+            string file1 = args[0];
+            string file2 = args[1];
+            int pKey = Int32.Parse(args[2]);
+
+            Console.WriteLine($"File1: {file1}, File2: {file2}, PrimaryKey: {pKey}");
+        }
+    }
+}
