ab96e5b54d63224bb55b90ff76089cfa7d8d8185
LC9er <liquidcloud9@gmail.com>
2022-07-07 21:52:33 -0400

    add todos to readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # csvd
 csv diffing tool
+
+# TODO
+
+1. Parse cli args.
+2. Open and read files.
+3. Populate dictionaries on primary key.
+4. Diff files.
+5. Diff files with field exclusions.
+6. Use Spectre to pretty print.
--- csvd/Program.cs
+++ csvd/Program.cs
@@ -1,18 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
 using CsvHelper;
 
 namespace csvd 
 {
     public class csvd
     {
+        /* public static Dictionary<string, string> CreateCsvDict(string file, int key) */
+        /* { */
+        /*     // Read csv file. Build dictionary with key */
+        /* } */
+
+        /* public static string[] GetKeys(Dictionary<string, string> csvDict) */
+        /* { */
+        /*     // return the keys in the dictionary */
+        /* } */
+
+        /* public static string[] GetExclusiveKeys(Dictionary<string, string> csvDict1, Dictionary<string, string> csvDict2) */
+        /* { */
+        /*     // return keys exclusive to csvDict1 */
+        /*     // Something like */ 
+        /* } */
+
         static void Main(string[] args)
         {
             /// Parse args - file1 file2 key
-            string file1 = args[0];
-            string file2 = args[1];
-            int pKey = Int32.Parse(args[2]);
+            /* string[] files = { args[0], args[1] }; */
+            /* int pKey = Int32.Parse(args[2]); */
 
-            Console.WriteLine($"File1: {file1}, File2: {file2}, PrimaryKey: {pKey}");
+            Dictionary<string, string> myDict1 = new Dictionary<string, string>();
+            Dictionary<string, string> myDict2 = new Dictionary<string, string>();
+
+            myDict1.Add("key1","value1");
+            myDict1.Add("key2","value2");
+            myDict2.Add("key1","value3");
+            myDict2.Add("key3","value8");
+            myDict1.Add("key4","value9");
+            myDict2.Add("key5","value8");
+
+            var keysDictionary1HasThat2DoesNot = myDict1.Keys.Except(myDict2.Keys);
+            var keysDictionary2HasThat1DoesNot = myDict2.Keys.Except(myDict1.Keys);
+
+            Console.WriteLine("Exclusive to Dictionary1: ");
+            foreach (var key in keysDictionary1HasThat2DoesNot)
+            {
+                Console.WriteLine(key.ToString());
+            }
+
+            Console.WriteLine("Exclusive to Dictionary2: ");
+            foreach (var key in keysDictionary2HasThat1DoesNot)
+            {
+                Console.WriteLine(key.ToString());
+            }
+
+
+            /* Console.WriteLine($"File1: {files[0]}, File2: {files[1]}, PrimaryKey: {pKey}"); */
+
+            // Build csvDicts
+
+            // Get Exclusive keys for each file (baseFile and deltaFile)
+
+            // Find keys in both
+            // Should be something like
+            // IEnumerable<string> both = myDict1.Keys.Intersect(myDict2.Keys)
+
+            // Check for value differences in 'both'
         }
     }
 }
